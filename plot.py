8b1ab858f1a7cad354e69b0c908f0f0e0f4372fa
krutovsky-danya <krutovsky.danya@gmail.com>
2023-01-04 22:12:41 +0500

    prettier points

--- plot.py
+++ plot.py
@@ -107,7 +107,7 @@ def show_attraction_pool(gamma: float, sigma: float, cycles_map, extent, *trajec
     fig, ax = plt.subplots()
     im = ax.imshow(cycles_map[::-1], extent=extent)
     for points in trajectories:
-        ax.plot(*points, '.')
+        ax.plot(*points, '.r', alpha=0.3)
     plt.title(f"$\\gamma={gamma:.4f}; \\sigma={sigma:.3f}$", size=20)
     plt.xlabel('$x$', size=20)
     plt.ylabel('$y$', size=20, rotation=0)
